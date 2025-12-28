1b6d13f3226c79c981bdc8996c11b10e0fc499c9
Haozhe Xu <xuhaozhepy@outlook.com>
2025-12-28 10:49:47 +0800

    Update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from typing import Union, List
 MAPPING = {}
 
 class Message:
-    def __init__(self, files:List[str], model, fragments:dict)->None:
+    def __init__(self, files:list, model:str, fragments:dict, *_, **__)->None:
         self.files = files if len(files) else None
         self.model = model
         self.msg = []
@@ -23,11 +23,11 @@ class Message:
 
 
 class TreeNode:
-    def __init__(self, id, parent, children, message, *args, **kwargs):
+    def __init__(self, id:str, parent:str, children:list, message:list, *_, **__):
         self.id = id
         self.parent = MAPPING.get(parent, None)
         self.children = [MAPPING.get(c) for c in children]
-        self.message = message
+        self.message = Message(**message)
     def add_child(self, child_node:Union[str, 'TreeNode']):
         self.children.append(child_node) if isinstance(child_node, TreeNode) else MAPPING.get(child_node)
     def __getitem__(self, key):
